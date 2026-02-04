4f206ce2a4bcb57f6106d11947a28c5bbfde2678
Fernando Pina Roldan <fpinaroldan@gmail.com>
2026-02-04 16:56:56 +0100

    Modulo de player terminado

---
+++ player.c
@@ -0,0 +1,155 @@
+/**
+ * @brief It implements the player module
+ *
+ * @file player.c
+ * @author Fernando Pina
+ * @version 0
+ * @date 04-02-2026
+ * @copyright GNU Public License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "types.h"
+#include "player.h"
+
+
+
+/**
+ * @brief player
+ *
+ * This struct stores all the information of a player.
+ */
+struct _Player {
+  Id id;                    /*!< Id number of the player, it must be unique */
+  char name[WORD_SIZE + 1]; /*!< Name of the player */
+  Id object;              /*!< Whether the player has an object or not */
+  Id location;              /*Id number o the location of the player*/
+};
+
+/** player_create allocates memory for a new player
+ *  and initializes its members
+ */
+Player* player_create(Id id) {
+  Player* newPlayer = NULL;
+
+  /* Error control */
+  if (id == NO_ID) return NULL;
+
+  newPlayer = (Player*)calloc(1, sizeof(Player));
+  if (newPlayer == NULL) {
+    return NULL;
+  }
+
+  /* Initialization of an empty space*/
+  newPlayer->id = id;
+  newPlayer->name[0] = '\0';
+  newPlayer->object = NO_ID;
+  newPlayer->location = NO_ID;
+
+  return newPlayer;
+}
+/*free the memory of the player*/
+Status player_destroy(Player* player) {
+  if (!player) {
+    return ERROR;
+  }
+
+  free(player); 
+  return OK;
+}
+
+/*returns player id*/
+Id player_get_id(Player* player) {
+  if (!player) {
+    return NO_ID;
+  }
+  return player->id;
+}
+
+/*it sets the player name*/
+Status player_set_name(Player* player, char* name) {
+  if (!player || !name) {
+    return ERROR;
+  }
+
+  if (!strcpy(player->name, name)) {
+    return ERROR;
+  }
+  return OK;
+}
+
+/*it returns the player name*/
+const char* player_get_name(Player* player) {
+  if (!player) {
+    return NULL;
+  }
+  return player->name;
+}
+
+
+/*set the object id*/
+Status player_set_object(Player* player, Id object_id) {
+  if (!player) {
+    return ERROR;
+  }
+  player->object = object_id;
+  return OK;
+}
+
+/*it returns the id of the object */
+Id player_get_object(Player* player) {
+  if (!player) {
+    return NO_ID;
+  }
+  return player->object;
+}
+
+/*it sets the player location*/
+Status player_set_location(Player* player, Id location_id) {
+  if (!player) {
+    return ERROR;
+  }
+  player->location = location_id;
+  return OK;
+}
+
+/*it returns the player id location*/
+Id player_get_location(Player* player) {
+  if (!player) {
+    return NO_ID;
+  }
+  return player->location;
+}
+
+
+Status player_print(Player* player) {
+  
+
+  /* Error Control */
+  if (!player) {
+    return ERROR;
+  }
+
+  /* 1. Print the id and the name of the player */
+  fprintf(stdout, "--> Space (Id: %ld; Name: %s)\n", player->id, player->name);
+
+  
+  /* 3. Print the object id if it exists else print a message if not */
+  if (player_get_object(player) != NO_ID) {
+    fprintf(stdout, "---> Object of the player (Id: %ld).\n", player->object);
+  } else {
+    fprintf(stdout, "---> The player has no object.\n");
+  }
+
+  if (player_get_location(player) != NO_ID) {
+    fprintf(stdout, "---> Location of the player (Id: %ld).\n", player->location);
+  } else {
+    fprintf(stdout, "---> no location.\n");
+  }
+
+
+  return OK;
+}
---
+++ player.h
@@ -0,0 +1,112 @@
+/**
+ * @brief It defines the player module interface
+ *
+ * @file player.h
+ * @author Fernando Pina
+ * @version 0
+ * @date 04-02-2026
+ * @copyright GNU Public License
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "types.h"
+
+typedef struct _Player Player;
+
+/**
+ * @brief It creates a new player, allocating memory and initializing its members
+ * @author Fernando Pina
+ *
+ * @param id the identification number for the new player
+ * @return a new player, initialized
+ */
+Player* player_create(Id id);
+
+/**
+ * @brief It destroys a player, freeing the allocated memory
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the player that must be destroyed
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status player_destroy(Player* player);
+
+/**
+ * @brief It gets the id of a player
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the Player
+ * @return the id of the player
+ */
+Id player_get_id(Player* player);
+
+/**
+ * @brief It sets the name of a player
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the player
+ * @param name a string with the name to store
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status player_set_name(Player* player, char* name);
+
+/**
+ * @brief It gets the name of a player
+ * @author Fernado Pina
+ *
+ * @param player a pointer to the player
+ * @return  a string with the name of the player
+ */
+const char* player_get_name(Player* player);
+
+/**
+ * @brief It sets whether the player has an object or not
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the player
+ * @param value a boolean, specifying if in the player there is an object (TRUE) or not (FALSE)
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status player_set_object(Player* player, Id object_id);
+
+/**
+ * @brief It gets whether the player has an object or not
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the player
+ * @return the id of the object*/
+Id player_get_object(Player* player);
+
+/**
+ * @brief It sets the location of the player
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the player
+ * @param location a id of the 
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+
+Status player_set_location(Player* player, Id location_id);
+
+/**
+ * @brief It gets the player location
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the player
+ * @return the id of the location of the player
+ */
+Id player_get_location(Player* player);
+
+/**
+ * @brief It prints the player information
+ * @author Fernando Pina
+ *
+ * This fucntion shows the id and name of the player,and if it has an object or not.
+ * @param player a pointer to the player
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status player_print(Player* player);
+
+#endif
