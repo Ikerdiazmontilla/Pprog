318b102a2889a02d657eea85f7979f2d9c4b768b
Fernando Pina Roldan <fpinaroldan@gmail.com>
2026-02-09 11:26:47 +0100

    Modulo object añadido

---
+++ object.c
@@ -0,0 +1,102 @@
+/**
+ * @brief It implements the object module
+ *
+ * @file object.c
+ * @author Fernando Pina
+ * @version 0
+ * @date 04-02-2026
+ * @copyright GNU Public License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "types.h"
+#include "object.h"
+
+
+
+/**
+ * @brief object
+ *
+ * This struct stores all the information of a object.
+ */
+struct _Object {
+  Id id;                    /*!< Id number of the object, it must be unique */
+  char name[WORD_SIZE + 1]; /*!< Name of the object */
+};
+
+/** player_create allocates memory for a new object
+ *  and initializes its members
+ */
+Object* object_create(Id id) {
+  Object* newObject = NULL;
+
+  /* Error control */
+  if (id == NO_ID) return NULL;
+
+  newObject = (Object*)calloc(1, sizeof(Object));
+  if (newObject == NULL) {
+    return NULL;
+  }
+
+  /* Initialization of an empty space*/
+  newObject->id = id;
+  newObject->name[0] = '\0';
+  
+  return newObject;
+}
+/*free the memory of the object*/
+Status object_destroy(Object* object) {
+  if (!object) {
+    return ERROR;
+  }
+
+  free(object); 
+  return OK;
+}
+
+/*returns object id*/
+Id object_get_id(Object* object) {
+  if (!object) {
+    return NO_ID;
+  }
+  return object->id;
+}
+
+/*it sets the object name*/
+Status object_set_name(Object* object, char* name) {
+  if (!object || !name) {
+    return ERROR;
+  }
+
+  if (!strcpy(object->name, name)) {
+    return ERROR;
+  }
+  return OK;
+}
+
+/*it returns the object name*/
+const char* object_get_name(Object* object) {
+  if (!object) {
+    return NULL;
+  }
+  return object->name;
+}
+
+
+Status object_print(Object* object) {
+  
+
+  /* Error Control */
+  if (!object) {
+    return ERROR;
+  }
+
+  /*  Print the id and the name of the object */
+  fprintf(stdout, "--> (Id: %ld; Name: %s)\n", object->id, object->name);
+
+
+  return OK;
+}
---
+++ object.h
@@ -0,0 +1,75 @@
+/**
+ * @brief It defines the object module interface
+ *
+ * @file object.h
+ * @author Fernando Pina
+ * @version 0
+ * @date 09-02-2026
+ * @copyright GNU Public License
+ */
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "types.h"
+
+typedef struct _Object Object;
+
+/**
+ * @brief It creates a new object, allocating memory and initializing its members
+ * @author Fernando Pina
+ *
+ * @param id the identification number for the object
+ * @return a new object, initialized
+ */
+Object* object_create(Id id);
+
+/**
+ * @brief It destroys a object, freeing the allocated memory
+ * @author Fernando Pina
+ *
+ * @param player a pointer to the object that must be destroyed
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status object_destroy(Object* object);
+
+/**
+ * @brief It gets the id of a object
+ * @author Fernando Pina
+ *
+ * @param object a pointer to the object
+ * @return the id of the object
+ */
+Id object_get_id(Object* object);
+
+/**
+ * @brief It sets the name of a object
+ * @author Fernando Pina
+ *
+ * @param object a pointer to the object
+ * @param name a string with the name to store
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status object_set_name(Object* object, char* name);
+
+/**
+ * @brief It gets the name of a object
+ * @author Fernado Pina
+ *
+ * @param object a pointer to the object
+ * @return  a string with the name of the object
+ */
+const char* object_get_name(Object* object);
+
+
+/**
+ * @brief It prints the object information
+ * @author Fernando Pina
+ *
+ * This fucntion shows the id and name of the object
+ * @param object a pointer to the object
+ * @return OK, if everything goes well or ERROR if there was some mistake
+ */
+Status object_print(Object* object);
+
+#endif
